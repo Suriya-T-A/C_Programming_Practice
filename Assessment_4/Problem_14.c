a339a5d1226d3041b657fd6732459cc7b838f559
Suriya-T-A <suriyata0306@gmail.com>
2026-06-21 22:27:08 +0530

    Assesment 04

--- Assessment_4/Problem_14.c
+++ Assessment_4/Problem_14.c
@@ -1,20 +1,3 @@
-/*#include<stdio.h>
-int main()
-{
-    int a,a1,b,c;
-    scanf("%d",&a);\
-    a1 = a;
-    b = a%10;
-    loop: if (a!=0)
-    {
-        c += 1;
-        a = a/10;
-        goto loop;
-    }
-    printf("%d",c);
-    return 0;
-}*/
-
 #include <stdio.h>
 
 int main() {
